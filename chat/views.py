03d98c489bf168f3e76e5fbdc72cb24162b11d8d
oouji <kyousukesouhei@gmail.com>
2024-04-14 16:02:13 +0900

    ２回目

--- chat/views.py
+++ chat/views.py
@@ -4,8 +4,8 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     #Home画面を表示するビュー
-    template_name = 'home.html'    #HomeというViewクラスではこのhtmlファイルを使用するということ
+    template_name = 'home.html'    #HomeというViewクラスではtemplatesフォルダ内のhtmlファイルを使用するということ
 
 class ChatRoom(TemplateView):
     #ChatRoomを表示するビュー
-    template_name = 'chat/chat_box.html'
+    template_name = 'chat/chat_box.html'   #tempatesフォルダ内のchatフォルダの中のchat_box.htmlを使用
